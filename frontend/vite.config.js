0a2b0c90c884030604b6c4c0128d0ec956836558
Al Mahmud Saif <132850000+AlMahmudSaif@users.noreply.github.com>
2025-11-17 23:18:20 +0600

    Modify Vite config to set server port
    Updated Vite configuration to include server port and removed API proxy settings.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "http://localhost:7000",
+//       },
+//     },
+//   },
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:7000",
-      },
-    },
+    port: 5173,
   },
-});
+})
