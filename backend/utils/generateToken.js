92d8622f0260e75821ae88326c7e935661d53e5c
Al Mahmud Saif <132850000+AlMahmudSaif@users.noreply.github.com>
2025-11-18 03:28:07 +0600

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -3,19 +3,10 @@
 
 import jwt from "jsonwebtoken";
 
-const generateToken = (id, res) => {
+const generateToken = (id) => {
   const token = jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "15d",
   });
-
-  res.cookie("token", token, {
-    maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
-    httpOnly: true,
-    sameSite: "none", // CHANGED: Allow cross-site cookies
-    secure: true,     // CHANGED: Always true for production
-    domain: ".onrender.com" // ADDED: Shared domain for subdomains
-  });
-
   return token;
 };
 
